f95fb46689ccf6081486dcae948fc3e283a159ea
Corey Ford <coyotebush22@gmail.com>
2013-11-10 13:44:48 -0800

    initial structure

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Key Mixer</title>
+</head>
+<body>
+  <h1>Key Mixer</h1>
+  <section>
+    <h2>Submit a key</h2>
+    <form action="" method="POST">
+      <textarea placeholder="ASCII-armored GPG data"></textarea>
+      <input type="submit">
+    </form>
+  </section>
+  <section>
+    <h2>Download the keyring</h2>
+    <a href="keyring.gpg">Download keyring</a>
+  </section>
+</body>
+</html>
+<!-- vim: set ts=2 sts=2 sw=2 et: -->
