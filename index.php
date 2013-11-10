213550a9b97fe8f70f1b2439cfddfffb766f327b
Corey Ford <coyotebush22@gmail.com>
2013-11-10 14:18:13 -0800

    tidy up form

--- index.php
+++ index.php
@@ -8,13 +8,15 @@
   <section>
     <h2>Submit a key</h2>
     <form action="" method="POST">
-      <textarea placeholder="ASCII-armored GPG data"></textarea>
-      <input type="submit">
+      <textarea placeholder="ASCII-armored GPG data" rows="10" cols="66"></textarea>
+      <p>
+        <input type="submit" value="Submit">
+      </p>
     </form>
   </section>
   <section>
     <h2>Download the keyring</h2>
-    <a href="keyring.gpg">Download keyring</a>
+    <a href="keyring.gpg">keyring.gpg</a>
   </section>
 </body>
 </html>
